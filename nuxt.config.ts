ab377c1c6da52d0f70d7bac38f316da8957205f7
claudiu.vimes <claudiu@whitecat.ro>
2025-10-15 15:27:53 +0300

    added stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['./app/assets/css/app.css'],
+  modules: ['@nuxt/ui','@nuxtjs/strapi'],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ro',
+      },
+    },
+  },
+  strapi: {
+    url: process.env.STRAPI_URL,
+    prefix: '/api',
+    admin: '/admin',
+    version: 'v4',
+    cookie: {
+      path: '/',
+      maxAge: 14 * 24 * 60 * 60,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: true,
+    },
+    cookieName: 'strapi_jwt'
+  },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+    theme: {
+        container: {
+            center: true,
+            padding: '1.25rem',
+        },
+        colors: {
+            primary: 'red',
+            tertiary: 'blue'
+        },
+    }
+}
